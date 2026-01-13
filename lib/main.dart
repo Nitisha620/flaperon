f411cc25355d90ef744936140fc4cb0c3122d65d
Nitisha620 <nitishajain94@gmail.com>
2026-01-13 16:25:45 +0530

    Added SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,8 @@ class _NavigationScreenState extends State<NavigationScreen> {
       getMarkerIcon(Colors.black,
           isIcon: true,
           icon: Icons.motorcycle_outlined,
-          iconColor: Colors.yellowAccent),
+          iconColor: Colors.yellowAccent, 
+          isStart: true),
       getMarkerIcon(const Color(0xFF007AFF),
           isIcon: true, icon: Icons.local_gas_station),
       getMarkerIcon(const Color(0xFF00C853), text: "S"),
@@ -156,238 +157,240 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // 1. Google Map
-          GoogleMap(
-            style: _mapStyle,
-            initialCameraPosition: const CameraPosition(
-              target: LatLng(
-                  26.205913876718085, 78.15043985986162), // Dummy coordinates
-              zoom: 14.8,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            // 1. Google Map
+            GoogleMap(
+              style: _mapStyle,
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(
+                    26.205913876718085, 78.15043985986162), // Dummy coordinates
+                zoom: 14.8,
+              ),
+              mapType: MapType.normal,
+              myLocationEnabled: true,
+              zoomControlsEnabled: false,
+              onMapCreated: (GoogleMapController controller) {},
+              polylines: {
+                Polyline(
+                    polylineId: const PolylineId("route1"),
+                    points: _getCurvedPoints(_roadPoints),
+                    color: Colors.yellowAccent.withAlpha(200),
+                    width: 4,
+                    jointType: JointType.round,
+                    startCap: Cap.roundCap,
+                    endCap: Cap.roundCap,
+                    geodesic: true),
+              },
+              markers: _markers,
             ),
-            mapType: MapType.normal,
-            myLocationEnabled: true,
-            zoomControlsEnabled: false,
-            onMapCreated: (GoogleMapController controller) {},
-            polylines: {
-              Polyline(
-                  polylineId: const PolylineId("route1"),
-                  points: _getCurvedPoints(_roadPoints),
-                  color: Colors.yellowAccent.withAlpha(200),
-                  width: 4,
-                  jointType: JointType.round,
-                  startCap: Cap.roundCap,
-                  endCap: Cap.roundCap,
-                  geodesic: true),
-            },
-            markers: _markers,
-          ),
-
-          // 2. Top Navigation Card
-          Positioned(
-            top: 50,
-            left: 20,
-            right: 20,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.zero,
-                      bottomRight: Radius.circular(25),
-                      topLeft: Radius.circular(25),
-                      topRight: Radius.circular(25),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withAlpha(100),
-                        blurRadius: 10,
-                        offset: const Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      const CircleAvatar(
-                        backgroundColor: Color(0xFF2A2A2A),
-                        child: Icon(Icons.arrow_upward, color: Colors.yellow),
-                      ),
-                      const SizedBox(width: 15),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "ABC Street",
-                            style: GoogleFonts.poppins(
-                              color: const Color(0xFF202124),
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20,
-                            ),
-                          ),
-                          Text(
-                            "Towards BCD Street",
-                            style: GoogleFonts.poppins(
-                              color: const Color(0xFF202124),
-                              fontWeight: FontWeight.w300,
-                              fontSize: 14,
-                              letterSpacing: -0.5,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  decoration: BoxDecoration(
+      
+            // 2. Top Navigation Card
+            Positioned(
+              top: 50,
+              left: 20,
+              right: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(25),
+                        bottomLeft: Radius.zero,
                         bottomRight: Radius.circular(25),
-                        topLeft: Radius.zero,
-                        topRight: Radius.zero,
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
                       ),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black.withAlpha(50),
+                          color: Colors.black.withAlpha(100),
                           blurRadius: 10,
                           offset: const Offset(0, 4),
                         ),
-                      ]),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text("Then take",
-                          style: GoogleFonts.manrope(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w500)),
-                      SizedBox(width: 5),
-                      Icon(Icons.turn_right, color: Colors.black),
-                    ],
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          backgroundColor: Color(0xFF2A2A2A),
+                          child: Icon(Icons.arrow_upward, color: Colors.yellow),
+                        ),
+                        const SizedBox(width: 15),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "ABC Street",
+                              style: GoogleFonts.poppins(
+                                color: const Color(0xFF202124),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "Towards BCD Street",
+                              style: GoogleFonts.poppins(
+                                color: const Color(0xFF202124),
+                                fontWeight: FontWeight.w300,
+                                fontSize: 14,
+                                letterSpacing: -0.5,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-
-          // 3. Floating Action Buttons (Right Side)
-          Positioned(
-            right: 12,
-            top: MediaQuery.of(context).size.height * 0.25,
-            child: Column(
-              children: [
-                _circularButton(Icons.sos, Colors.red,
-                    label: "SOS", isText: true),
-                const SizedBox(height: 20),
-                _circularButton(
-                  Icons.keyboard_arrow_down,
-                  Colors.white,
-                ),
-              ],
-            ),
-          ),
-
-          // 4. Speedometer (Bottom Left)
-          Positioned(
-            left: 20,
-            bottom: 120,
-            child: Container(
-              padding: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(25),
+                          bottomRight: Radius.circular(25),
+                          topLeft: Radius.zero,
+                          topRight: Radius.zero,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withAlpha(50),
+                            blurRadius: 10,
+                            offset: const Offset(0, 4),
+                          ),
+                        ]),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("Then take",
+                            style: GoogleFonts.manrope(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500)),
+                        SizedBox(width: 5),
+                        Icon(Icons.turn_right, color: Colors.black),
+                      ],
+                    ),
+                  ),
+                ],
               ),
+            ),
+      
+            // 3. Floating Action Buttons (Right Side)
+            Positioned(
+              right: 12,
+              top: MediaQuery.of(context).size.height * 0.25,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("168",
-                      style: GoogleFonts.manrope(
-                          color: Colors.red[900],
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18)),
-                  Text("km/h",
-                      style: GoogleFonts.manrope(
-                          color: Colors.grey,
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-          ),
-
-          // 5. Compass (Bottom Right)
-          Positioned(
-            right: 20,
-            bottom: 130,
-            child: _circularButton(Icons.explore_outlined, Colors.white),
-          ),
-
-          // 6. Persistent Navigation Overlay
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
-                boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Exit Button (Close)
-                  _iconCircle(
-                    Icons.close,
-                  ),
-                  // Text Info
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        width: 40,
-                        height: 4,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        "2hr 51min",
-                        style: GoogleFonts.manrope(
-                          color: const ui.Color.fromARGB(255, 41, 191, 83),
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        "650 km • 2:40 am",
-                        style: GoogleFonts.manrope(
-                          color: const Color(0xFF70757A),
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // Alternative Route
-                  _iconCircle(
-                    Icons.call_split,
+                  _circularButton(Icons.sos, Colors.red,
+                      label: "SOS", isText: true),
+                  const SizedBox(height: 20),
+                  _circularButton(
+                    Icons.keyboard_arrow_down,
+                    Colors.white,
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+      
+            // 4. Speedometer (Bottom Left)
+            Positioned(
+              left: 20,
+              bottom: 120,
+              child: Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("168",
+                        style: GoogleFonts.manrope(
+                            color: Colors.red[900],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18)),
+                    Text("km/h",
+                        style: GoogleFonts.manrope(
+                            color: Colors.grey,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ),
+      
+            // 5. Compass (Bottom Right)
+            Positioned(
+              right: 20,
+              bottom: 130,
+              child: _circularButton(Icons.explore_outlined, Colors.white),
+            ),
+      
+            // 6. Persistent Navigation Overlay
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+                  boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Exit Button (Close)
+                    _iconCircle(
+                      Icons.close,
+                    ),
+                    // Text Info
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          width: 40,
+                          height: 4,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          "2hr 51min",
+                          style: GoogleFonts.manrope(
+                            color: const ui.Color.fromARGB(255, 41, 191, 83),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          "650 km • 2:40 am",
+                          style: GoogleFonts.manrope(
+                            color: const Color(0xFF70757A),
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+      
+                    // Alternative Route
+                    _iconCircle(
+                      Icons.call_split,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
