a74d0f112ba7fb22139db886bbd45870e9584e96
Nitisha620 <nitishajain94@gmail.com>
2026-01-26 21:04:28 +0530

    Added app.dart instead of having logic in the main file

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flaperon/app/router/app_router.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        routerConfig: appRouter);
+  }
+}
